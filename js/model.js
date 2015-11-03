d1ce5e22b58739b7953ae312624f7f4f6dd35e00
Herr Paul <weichwarenerpel@gmail.com>
2015-11-03 23:06:45 +0100

    date/time getrennt

--- js/model.js
+++ js/model.js
@@ -2,7 +2,7 @@
 
 function MainModel() {
 	var self = this;
-    self.orders = ko.observableArray();
+	self.orders = ko.observableArray();
 	self.customers = [
 		new Customer("Peter Fischer GmbH", [ new Machine("Fräse"), new Machine("Drucker"), new Machine("Presse") ]),
 		new Customer("Hans Dampf Ldt.", [ new Machine("Pumpe"), new Machine("Säge") ]),
@@ -32,15 +32,29 @@ function Machine(name) {
 };
 
 function Task() {
-	this.part = null;
-	this.start = null;
-	this.end = null;
+	this.part = ko.observable();
+	this.startDate = ko.observable();
+	this.startTime = ko.observable();
+	this.endDate = ko.observable();
+	this.endTime = ko.observable();
 };
 
 ko.applyBindings(new MainModel());
 
 ko.bindingHandlers.datePicker = {
-    init: function(element, valueAccessor) {
-        $(element).datetimepicker();
+	init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+		var options = allBindings().params || {};
+		options.onChangeDateTime = function(dp, input) {
+			var value = valueAccessor();
+			value(input.val());
+		};
+		options.startDate = ko.unwrap(valueAccessor());
+		ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+			$(element).datetimepicker("destroy");
+		});
+		$(element).datetimepicker(options);
     },
+    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+
+    }
 };
